2fb779b4e51a254abc4c31e858d1247813de6aeb
Mohammed Adenwala <adenwala@outlook.com>
2021-03-03 13:38:13 -0600

    Add project files.

---
+++ Mobile/Contoso.Mobile.Core/ViewModels/BaseViewModel.cs
@@ -0,0 +1,83 @@
+﻿using Contoso.Mobile.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Contoso.Mobile.Core.ViewModels
+{
+    public abstract class BaseViewModel : BaseModel
+    {
+        #region Properties
+
+        private bool _StatusIsBusy;
+        public bool StatusIsBusy
+        {
+            get { return _StatusIsBusy; }
+            private set { this.SetProperty(ref _StatusIsBusy, value); }
+        }
+
+        private string _StatusText;
+        public string StatusText
+        {
+            get { return _StatusText; }
+            private set { this.SetProperty(ref _StatusText, value); }
+        }
+
+        private Command _RefreshCommand;
+        public Command RefreshCommand
+        {
+            get { return _RefreshCommand; }
+            private set { this.SetProperty(ref _RefreshCommand, value); }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public BaseViewModel()
+        {
+            this.RefreshCommand = new Command(async () => await RefreshAsync(true, this.StatusText));
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected void ShowBusyStatus(string statusMessage)
+        {
+            this.StatusIsBusy = true;
+
+            if(!string.IsNullOrWhiteSpace(statusMessage))
+                this.StatusText = statusMessage;
+        }
+
+        protected void ClearStatus()
+        {
+            this.StatusIsBusy = false;
+            this.StatusText = null;
+        }
+
+        public async Task RefreshAsync(bool forceRefresh = false, string statusText = null)
+        {
+            try
+            {
+                this.ShowBusyStatus(statusText);
+                await this.OnRefreshAsync(forceRefresh);
+            }
+            finally
+            {
+                this.ClearStatus();
+            }
+        }
+
+        protected virtual Task OnRefreshAsync(bool forceRefresh)
+        {
+            return Task.CompletedTask;
+        }
+
+        #endregion
+    }
+}
