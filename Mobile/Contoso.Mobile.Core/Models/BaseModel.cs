0684a5a4b6b24d0e04a6fcf12fbd360cffbede08
Mohammed Adenwala <adenwala@outlook.com>
2021-03-04 23:49:56 -0600

    BaseModel NotifyPropertyChanged has expression type

--- Mobile/Contoso.Mobile.Core/Models/BaseModel.cs
+++ Mobile/Contoso.Mobile.Core/Models/BaseModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 
 namespace Contoso.Mobile.Core.Models
@@ -23,9 +24,37 @@ namespace Contoso.Mobile.Core.Models
             }
         }
 
-        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        protected internal void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
         {
             this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void NotifyPropertyChanged<T>(Expression<Func<T>> property)
+        {
+            var propertyName = this.GetPropertyName<T>(property);
+            this.NotifyPropertyChanged(propertyName);
+        }
+
+        private string GetPropertyName<T>(Expression<Func<T>> property)
+        {
+            MemberExpression memberExpression = GetMememberExpression<T>(property);
+            return memberExpression.Member.Name;
+        }
+
+        private MemberExpression GetMememberExpression<T>(Expression<Func<T>> property)
+        {
+            var lambda = (LambdaExpression)property;
+
+            MemberExpression memberExpression;
+            if (lambda.Body is UnaryExpression)
+            {
+                var unaryExpression = (UnaryExpression)lambda.Body;
+                memberExpression = (MemberExpression)unaryExpression.Operand;
+            }
+            else
+                memberExpression = (MemberExpression)lambda.Body;
+
+            return memberExpression;
         }
     }
 }
